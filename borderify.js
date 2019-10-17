2bcaae16ea3dce80b633d054b7d0d2481f2bc154
Jesse Hill <j_hill14@u.pacific.edu>
2019-10-17 12:21:59 -0400

    Counts number of no test available messages, website changed to myUI

--- borderify.js
+++ borderify.js
@@ -2,6 +2,8 @@ document.body.style.border = "5px solid blue";
 var count = parseInt(window.sessionStorage.getItem("countKey"));
 var refreshTimer = setTimeout(refresher, 5000);
 window.addEventListener("beforeunload", clearvars);
+var testText = "No available tests match your demographics at this time";
+
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
@@ -12,8 +14,27 @@ function sleep(milliseconds) {
   }
 }
 
+function textCounter(){
+	console.log("Text Counter 3");
+	// swap  c-primary... with "ntah"
+	var searchtext = document.getElementsByClassName("ntah");
+	var occurences = searchtext.length;
+	console.log("OCCURENCES: ");
+	console.log(occurences);
+	if (occurences >= 2){
+		console.log("No available tests");
+	}else{
+		console.log("tests available");
+	}
+
+	sleep(3000);
+
+
+}
+
 function refresher(){
 	console.log("refresher");
+	textCounter();
 	if (isNaN(count)){
 		console.log("is null");
 		count = 1;
